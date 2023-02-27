22acd407c2173c6d353f3dd34ee2d1d103230147
yolly98 <gianlucagemini98@gmail.com>
2023-02-27 21:00:48 +0100

    completed list packets

--- src/packet/List.h
+++ src/packet/List.h
@@ -5,8 +5,11 @@
 #include <string>
 #include <cstdint>
 #include <cstring>
+#include <openssl/rand.h>
 
 #include "./CommandCodes.h"
+
+#define FILE_NAME_SIZE 30 //TODO valutare se metterlo da un altra parte
 
 using namespace std;
 
@@ -17,17 +20,51 @@ struct ListM1 {
     uint8_t command_code;
     uint32_t counter;
 
+
     ListM1() {}
 
-    ListM1(uint32_t counter, string file_name) {}
+    ListM1(uint32_t counter) {
+        this->command_code = FILE_LIST_REQ;
+        this->counter = counter;
+    }
 
-    uint8_t* serialize() const { return nullptr; }
+    uint8_t* serialize() const {
 
-    static ListM1 deserialize(uint8_t* buffer) { return ListM1(); }
+        uint8_t* buffer = new uint8_t[COMMAND_FIELD_PACKET_SIZE];
 
-    static int getSize() { return 0; }
+        size_t position = 0;
+        memcpy(buffer, &command_code, sizeof(uint8_t));
+        position += sizeof(uint8_t);
 
-    void print() const {}
+        memcpy(buffer + position, &counter, sizeof(uint32_t));
+        position += sizeof(uint32_t);
+
+        // add random bytes
+        RAND_bytes(buffer + position, COMMAND_FIELD_PACKET_SIZE - position);
+
+        return buffer;
+    }
+
+    static ListM1 deserialize(uint8_t* buffer) {
+
+        ListM1 listM1;
+
+        size_t position = 0;
+        memcpy(&listM1.command_code, buffer, sizeof(uint8_t));
+        position += sizeof(uint8_t);
+
+        memcpy(&listM1.counter, buffer + position, sizeof(uint32_t));
+
+        return listM1;
+    }
+
+    void print() const {
+
+        cout << "--------- LIST M1 ----------" << endl;
+        cout << "COMMAND CODE: " << printCommandCodeDescription(command_code) << endl;
+        cout << "COUNTER: " << counter << endl;
+        cout << "------------------------------" << endl;
+    }
 };
 
 // ------------------------------------ LIST M2 -------------------------------------
@@ -40,15 +77,65 @@ struct ListM2 {
 
     ListM2() {}
 
-    ListM2(uint32_t counter, string file_name) {}
+    ListM2(uint32_t counter, uint32_t file_list_size) {
 
-    uint8_t* serialize() const { return nullptr; }
+        this->command_code = FILE_LIST_SIZE;
+        this->counter = counter;
+        this->file_list_size = file_list_size;
+    }
 
-    static ListM2 deserialize(uint8_t* buffer) { return ListM2(); }
+    uint8_t* serialize() const { 
 
-    static int getSize() { return 0; }
+        uint8_t* buffer = new uint8_t[ListM2::getSize()];
 
-    void print() const {}
+        size_t position = 0;
+        memcpy(buffer, &command_code, sizeof(uint8_t));
+        position += sizeof(uint8_t);
+
+        memcpy(buffer + position, &counter, sizeof(uint32_t));
+        position += sizeof(uint32_t);
+
+        memcpy(buffer + position, &file_list_size, sizeof(uint32_t));
+    
+        return buffer;
+
+    }
+
+    static ListM2 deserialize(uint8_t* buffer) {
+
+        ListM2 listM2;
+
+        size_t position = 0;
+        memcpy(&listM2.command_code, buffer, sizeof(uint8_t));
+        position += sizeof(uint8_t);
+
+        memcpy(&listM2.counter, buffer + position, sizeof(uint32_t));
+        position += sizeof(uint32_t);
+
+        memcpy(&listM2.file_list_size, buffer + position, sizeof(uint32_t));
+
+        return listM2;
+    }
+
+    static int getSize() {
+        
+        int size = 0;
+
+        size += sizeof(uint8_t);
+        size += sizeof(uint32_t);
+        size += sizeof(uint32_t);
+
+        return size;
+    }
+
+    void print() const {
+
+        cout << "--------- LIST M2 ----------" << endl;
+        cout << "COMMAND CODE: " << printCommandCodeDescription(command_code) << endl;
+        cout << "COUNTER: " << counter << endl;
+        cout << "FILE SIZE: " << file_list_size << endl;
+        cout << "------------------------------" << endl;
+    }
 };
 
 // ------------------------------------ LIST M3 -------------------------------------
@@ -57,21 +144,79 @@ struct ListM3 {
 
     uint8_t command_code;
     uint32_t counter;
-    char* available_files;          // size = (file_list_size * 30) + (file_list_size - 1)
+    uint8_t* available_files;          // size = (file_list_size * 30) + (file_list_size - 1)
+    int available_files_size;
 
     ListM3() {}
 
-    ListM3(uint32_t counter, string file_name) {}
+    ListM3(uint32_t counter, uint8_t* available_files, uint32_t file_list_size) {
+
+        int available_files_size = (file_list_size * FILE_NAME_SIZE) + (file_list_size - 1);
+        
+        this->command_code = FILE_LIST;
+        this->counter = counter;
+        this->available_files_size = available_files_size;
+        memcpy(this->available_files, available_files, available_files_size * sizeof(uint8_t));
+
+    }
 
     ~ListM3() { delete[] available_files; }
 
-    uint8_t* serialize() const { return nullptr; }
+    uint8_t* serialize() const {
 
-    static ListM3 deserialize(uint8_t* buffer) { return ListM3(); }
+        int buffer_size = sizeof(uint8_t) + sizeof(uint32_t) + (available_files_size * sizeof(uint8_t));
+        uint8_t* buffer = new uint8_t[buffer_size];
 
-    static int getSize() { return 0; }
+        size_t position = 0;
+        memcpy(buffer, &command_code, sizeof(uint8_t));
+        position += sizeof(uint8_t);
 
-    void print() const {}
+        memcpy(buffer + position, &counter, sizeof(uint32_t));
+        position += sizeof(uint32_t);
+
+        memcpy(buffer + position, available_files, available_files_size * sizeof(uint8_t));
+
+        return buffer;
+
+    }
+
+    static ListM3 deserialize(uint8_t* buffer, int buffer_size) {
+
+        ListM3 listM3;
+        listM3.available_files_size = buffer_size - (sizeof(uint8_t) + sizeof(uint32_t));
+
+        size_t position = 0;
+        memcpy(&listM3.command_code, buffer, sizeof(uint8_t));
+        position += sizeof(uint8_t);
+
+        memcpy(&listM3.counter, buffer + position, sizeof(uint32_t));
+        position += sizeof(uint32_t);
+
+        memcpy(&listM3.available_files, buffer + position, listM3.available_files_size * sizeof(uint8_t));
+
+        return listM3;
+    }
+
+    static int getSize(uint32_t file_list_size) {
+
+        int size = 0;
+        
+        size += sizeof(uint8_t);
+        size += sizeof(uint32_t);
+        size += (file_list_size * FILE_NAME_SIZE) + (file_list_size - 1);
+
+        return size;
+    }
+
+    void print() const {
+        cout << "--------- LIST M3 ----------" << endl;
+        cout << "COMMAND CODE: " << printCommandCodeDescription(command_code) << endl;
+        cout << "COUNTER: " << counter << endl;
+        for (int i = 0; i < available_files_size; ++i)
+            cout << (char)available_files;
+        cout << endl;
+        cout << "------------------------------" << endl;
+    }
 };
 
 // ----------------------------------------------------------------------------------
